9c6322125d4fab0aaf6683501eccd89b54fa44fc
Alina  Lubiana <alina.furman28@gmail.com>
2022-10-30 20:04:48 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 'use strict';
 
 function titleClickHandler(event){
+ event.preventDefault(); /* щоб не змінювалась адреса сайту при натисканні на посиланння */
   const clickedElement = this;
   console.log('Link was clicked!');
+  
 
   /* [DONE] remove class 'active' from all article links 
   видалити клас «активний» з усіх посилань на статті */
@@ -13,13 +15,15 @@ function titleClickHandler(event){
     activeLink.classList.remove('active');
   }
 
-  /*[IN PROGRESS] add class 'active' to the clicked link
+
+  /* [DONE] add class 'active' to the clicked link
   додайте клас «активний» до натиснутого посилання */
 
   clickedElement.classList.add('active');
   console.log('clickedElement:', clickedElement);
   
-  /*[DONE] remove class 'active' from all articles
+
+  /* [DONE] remove class 'active' from all articles
   видалити клас «активний» з усіх статей */
 
   const activeArticles = document.querySelectorAll('article.post');
@@ -27,14 +31,26 @@ function titleClickHandler(event){
   for(let activeArticle of activeArticles){
     activeArticle.classList.remove('active');
   }
-  /* get 'href' attribute from the clicked link
-  отримати атрибут 'href' за натиснутим посиланням */
 
-  /* find the correct article using the selector (value of 'href' attribute)
+
+  /* [DONE] get 'href' attribute from the clicked link
+  отримати атрибут 'href' за натиснутим посиланням */
+  const articleSelector = clickedElement.getAttribute('href');
+  console.log ('my articleSelectors', clickedElement.getAttribute('href'));
+
+
+  /* [DONE] find the correct article using the selector (value of 'href' attribute)
   знайти правильну статтю за допомогою селектора (значення атрибута 'href') */
 
-  /* add class 'active' to the correct article
+  const targetArticle = document.querySelector(articleSelector);
+  console.log ('my targetArticle', targetArticle);
+ 
+ 
+  /* [DONE]  add class 'active' to the correct article
   додати клас "активний" до правильної статті */
+
+  targetArticle.classList.add('active');
+
 }
 
 const links = document.querySelectorAll('.titles a');
@@ -42,6 +58,7 @@ const links = document.querySelectorAll('.titles a');
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
 }
+
 
 
 
