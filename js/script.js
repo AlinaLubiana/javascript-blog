72de6dc0ef884ddd0a989ca1e15ea4a9fbabfb10
Alina Lubiana <alina.furman28@gmail.com>
2022-11-17 11:02:07 +0100

    add find all articles

--- js/script.js
+++ js/script.js
@@ -59,6 +59,50 @@ for(let link of links){
   link.addEventListener('click', titleClickHandler);
 }
 
+function generateTags(){
+  /* find all articles 
+  знайти всі статті*/
+  
+  const optArticleSelector = '.post',
+    optTitleSelector = '.post-title',
+    optTitleListSelector = '.titles',
+    optArticleTagsSelector = '.post-tags .list';
 
+
+  /* START LOOP: for every article: 
+  ПОЧАТОК ЦИКЛУ: для кожної статті:*/
+
+    /* find tags wrapper
+    обгортка пошуку тегів*/
+
+    /* make html variable with empty string
+    створити змінну html із порожнім рядком */
+
+    /* get tags from data-tags attribute
+    отримати теги з атрибута data-tags*/
+
+    /* split tags into array 
+    розділити теги на масив*/
+
+    /* START LOOP: for each tag
+    START LOOP: для кожного тегу */
+
+      /* generate HTML of the link 
+      створити HTML посилання*/
+
+      /* add generated code to html variable
+      додати згенерований код до змінної html */
+
+    /* END LOOP: for each tag 
+    ЗАКІНЧИТИ ЦИКЛ: для кожного тегу*/
+
+    /* insert HTML of all the links into the tags wrapper
+    вставте HTML усіх посилань у оболонку тегів */
+
+  /* END LOOP: for every article: 
+  ЗАКІНЧИТИ ЦИКЛ:  для кожної статті:*/
+}
+
+generateTags();
 
 
